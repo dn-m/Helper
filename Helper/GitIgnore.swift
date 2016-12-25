d4d17c567ccd870e0257bd3cc17191393bf33632
jsbean <jbean46@gmail.com>
2016-12-25 19:20:59 +0100

    Implement .gitignore get

--- Helper/GitIgnore.swift
+++ Helper/GitIgnore.swift
@@ -8,4 +8,42 @@
 
 import Foundation
 
-// TODO
+/// Create a `.gitignore` for a in the given `directory`.
+internal func createGitIgnore(in directory: URL) throws {
+    let contents = prepareGitIgnoreContents()
+    try createFile(name: ".gitignore", in: directory, contents: contents)
+}
+
+/// - returns: `String` contents of `.gitignore`.
+private func prepareGitIgnoreContents() -> String {
+    var result = ""
+    result.line("## Build generated")
+    result.line("build/")
+    result.line("DerivedData/")
+    result.line()
+    result.line("## Various settings")
+    result.line("*.pbxuser")
+    result.line("!default.pbxuser")
+    result.line("*.mode1v3")
+    result.line("!default.mode1v3")
+    result.line("*.mode2v3")
+    result.line("!default.mode2v3")
+    result.line("*.perspectivev3")
+    result.line("!default.perspectivev3")
+    result.line("xcuserdata/")
+    result.line()
+    result.line("## Other")
+    result.line("*.moved-aside")
+    result.line("*.xcuserstate")
+    result.line()
+    result.line("## Obj-C/Swift specific")
+    result.line("*.hmap")
+    result.line("*.ipa")
+    result.line("*.dSYM.zip")
+    result.line("*.dSYM")
+    result.line()
+    result.line("# Carthage")
+    result.line("Carthage/Checkouts")
+    result.line("Carthage/Build")
+    return result
+}
