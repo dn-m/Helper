185e99646b3627d1c88575bfc7500bfecc884404
jsbean <jbean46@gmail.com>
2016-12-25 17:06:44 +0100

    Create project, add gitignore

---
+++ Helper/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Helper
+//
+//  Created by James Bean on 12/25/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
